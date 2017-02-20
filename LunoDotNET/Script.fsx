dd1effd9637c06b5a6b7d19a7b6bb854fa1826fe
Christoph <ideaflare@gmail.com>
2017-02-20 22:25:58 +0200

    Copy example function.

--- LunoDotNET/Script.fsx
+++ LunoDotNET/Script.fsx
@@ -1,17 +1,15 @@
 open System
 
-let pchar (charToMatch, str) =
-    let charListToString x = x |> List.toArray |> System.String
+let pchar (charToMatch,str) =
     if String.IsNullOrEmpty(str) then
         let msg = "No more input"
-        (msg, "")
-    else
-        match (str |> Seq.toList) with
-        | found :: remaining when found = charToMatch -> 
+        (msg,"")
+    else 
+        let first = str.[0] 
+        if first = charToMatch then
+            let remaining = str.[1..]
             let msg = sprintf "Found %c" charToMatch
-            (msg, charListToString remaining)
-        | first :: _ ->
+            (msg,remaining)
+        else
             let msg = sprintf "Expecting '%c'. Got '%c'" charToMatch first
             (msg,str)
-        | [] ->
-            ("Nothing there", str)
