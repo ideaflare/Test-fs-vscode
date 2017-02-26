850e551227ed3a9119b4d33a337ca9353c2793e8
Christoph <ideaflare@gmail.com>
2017-02-26 23:07:22 +0200

    Add andThen parser combinator

--- LunoDotNET/Script.fsx
+++ LunoDotNET/Script.fsx
@@ -4,7 +4,10 @@ type Result<'a> =
     | Success of 'a
     | Failure of string
 
-type Parser<'a> = Parser of (string -> Result<'a * string>)
+ type Parser<'r> = Parser of (string -> Result<'r * string>)
+// Also works @ Implementation 5
+// type Parser = Parser of (string -> Result<char * string>)
+
 //-------------------------------------------------------------
 
 let pchar charToMatch =
@@ -25,3 +28,27 @@ let pchar charToMatch =
 let run parser str =
     let (Parser innerFn) = parser
     innerFn str
+
+let andThen parser1 parser2 =
+    let innerFn input =
+        match (run parser1 input) with
+        | Failure err -> Failure err
+        | Success (value1, remaining1) ->
+            match (run parser2 remaining1) with
+            | Failure err -> Failure err
+            | Success (value2, remaining2) ->
+                let newValue = (value1,value2)
+                Success (newValue, remaining2)
+    Parser innerFn
+
+let (.>>.) = andThen
+
+let parseA = pchar 'A'
+let parseB = pchar 'B'
+let parseAThenB = parseA .>>. parseB
+
+run parseAThenB "ABThe rest"  // Success (('A', 'B'), "The rest")
+
+run parseAThenB "ZBC"  // Failure "Expecting 'A'. Got 'Z'"
+
+run parseAThenB "AZC"  // Failure "Expecting 'B'. Got 'Z'"
